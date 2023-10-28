93661fba7d0e851e1f5e3d5498c35a701072e97a
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-10-28 16:06:36 +0530

    Python ContainsDuplicate Solution

---
+++ Python/Arrays and Hashing/ContainsDuplicate.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #ans = False;
+
+        seen = {};
+
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True;
+            seen[num] = seen.get(num, 0) + 1
+        return False;
+
+
+
+#define bool - with uppercase first letter - True and False
