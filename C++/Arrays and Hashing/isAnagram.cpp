2a3d3526ebbfecb8f939fb33d20a6c19412dc528
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-10-31 23:51:00 +0530

    isAnagram cpp solution

---
+++ C++/Arrays and Hashing/isAnagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s.compare(t)!=0)
+            return false;
+        return true;
+    }
+};
+
+#sort both of them and compare strings
