19fb6bbe595a4b60df0d31a4fbf5aa579746725d
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-11-01 00:05:03 +0530

    isAnagram python solution

--- Python/Arrays and Hashing/isAnagram.py
+++ Python/Arrays and Hashing/isAnagram.py
@@ -10,3 +10,11 @@ class Solution:
 
 #sort a string in python
 #check in-place sorting as well
+
+#Sol 1.2 - other way to sort in python
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        return sorted_s == sorted_t
