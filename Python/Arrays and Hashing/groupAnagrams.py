26dc65d6b897dd3a008570f2eaa64cd8e35eb779
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2024-01-19 19:35:33 +0530

    group anagrams python better solution

---
+++ Python/Arrays and Hashing/groupAnagrams.py
@@ -0,0 +1,42 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list);  #Initialize hashmap of lists
+        for stemp in strs:
+            res = ''.join(sorted(stemp))
+            #print(res);
+            if res in hashmap:
+                hashmap[res].append(stemp)
+                #print("if-",stemp)
+            else:
+                hashmap[res].append(stemp)
+                #print("else-",stemp)
+        #print(hashmap)
+        ans = []  #initialize list of lists
+        for key,value in hashmap.items():  #iterate hashmap of lists
+            ans.append(value)
+
+        return ans
+
+
+
+#from collections import defaultdict
+
+# Create a defaultdict with lists as default values
+#hashmap_of_lists = defaultdict(list)
+
+
+# Iterate over the hashmap of lists
+#for key, values in hashmap_of_lists.items():
+#    print(f"Key: {key}, Values: {values}")
+
+
+#Better solution
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+
+        return list(anagram_map.values())
