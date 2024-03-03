b5c3b67e5d84a6f466e2ef14c3329f6ccefabced
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2024-03-03 16:28:41 +0530

    Find min sorted array solution py and cpp

--- Python/Binary Search/Koko banana.py
+++ Python/Binary Search/Koko banana.py
@@ -1,11 +1,12 @@
+def something(k: int, piles: List[int], h: int) -> bool:
+    total_time=0
+    for pile in piles:
+        total_time+= (pile-1)//k +1
+    return total_time <= h
+
 class Solution:
 
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def something(k: int, piles: List[int], h: int) -> bool:
-            total_time=0
-            for pile in piles:
-                total_time+= (pile-1)//k +1
-            return total_time <= h
 
         n = len(piles)
         left, right = 1, max(piles)
@@ -20,3 +21,5 @@ class Solution:
                 left = mid + 1
 
         return left
+
+    #note here defining a new method should be outside of solution class
